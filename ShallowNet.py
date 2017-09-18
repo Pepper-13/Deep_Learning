e2b4920665ed388090b82854444cb5de76390353
Pepper-13 <pepper_1317@yahoo.com>
2017-09-18 23:03:28 +0530

    Update ShallowNet.py

--- ShallowNet.py
+++ ShallowNet.py
@@ -1,3 +1,10 @@
 #Set seed for reproducibility 
 import numpy as np
 np.random.seed(42)
+
+#Load dependencies
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
