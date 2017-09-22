11eb61480b1e8a7f1087cf849a0007a48400874c
Pepper-13 <pepper_1317@yahoo.com>
2017-09-22 07:56:09 +0530

    Update deepNeuralNet.py

--- deepNeuralNet.py
+++ deepNeuralNet.py
@@ -2,4 +2,15 @@
 import numpy as np
 np.random.seed(42)
 
+#LoadDependency
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout # new!
+from keras.layers.normalization import BatchNormalization # new!
+from keras import regularizers # new! 
+from keras.optimizers import SGD
+
+
 
