d8ce790cd1ebfa8f490e51c00bdeab84de67310d
Pepper-13 <pepper_1317@yahoo.com>
2017-09-22 07:56:25 +0530

    Update deepNeuralNet.py

--- deepNeuralNet.py
+++ deepNeuralNet.py
@@ -12,5 +12,6 @@ from keras.layers.normalization import BatchNormalization # new!
 from keras import regularizers # new! 
 from keras.optimizers import SGD
 
-
+#Load_data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
