4b3883e39080b09fcf430fc3b79b72064862fdaa
Pepper-13 <pepper_1317@yahoo.com>
2017-09-22 07:58:17 +0530

    Update deepNeuralNet.py

--- deepNeuralNet.py
+++ deepNeuralNet.py
@@ -15,3 +15,6 @@ from keras.optimizers import SGD
 #Load_data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
+#Prepocess data
+X_train = X_train.reshape(60000, 784).astype('float32')
+X_test = X_test.reshape(10000, 784).astype('float32')
