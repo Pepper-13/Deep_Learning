ef3df1bcc5c3cd8f138e0b94a7eb207eb8911e86
Pepper-13 <pepper_1317@yahoo.com>
2017-09-23 14:32:09 +0530

    Create BestPractises_NLP.py

---
+++ BestPractises_NLP.py
@@ -0,0 +1,12 @@
+
+#Load Dependencies
+import nltk
+from nltk import word_tokenize, sent_tokenize
+import gensim
+from gensim.models.word2vec import Word2Vec
+from sklearn.manifold import TSNE
+import pandas as pd
+from bokeh.io import output_notebook, output_file
+from bokeh.plotting import show, figure
+%matplotlib inline
+
